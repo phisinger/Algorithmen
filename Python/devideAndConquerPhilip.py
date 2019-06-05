d3ec0c55aa1aabd87c484d73d4bd2e7ec09808e2
phisinger <philipsinger@mail.de>
2019-06-05 06:24:54 +0200

    BinSearch

--- Python/devideAndConquerPhilip.py
+++ Python/devideAndConquerPhilip.py
@@ -1,19 +1,32 @@
+import sys
 liste = [1,2,3,4,5,7,8,9,10,11,13,14]
 
-def dAC(liste, key):
-    print(liste)
-    un = liste[0]
-    ob = liste[-1]
-    mid = liste[len(liste)/2]
 
-    while (liste[0] <= liste[-1]):
-        if(key == Liste[0]):
-            return liste[0]
-        elif(key < mid):
-            dAC(liste[:mid], key)
-        else:
-            dAC(liste[mid:], key)
+def dAC(liste, unten, oben, key):
+    if (oben < unten):
+        return None
+
+    mid = (unten + oben) // 2
+    print(liste[mid])
+    if(key == liste[mid]):
+        return mid
+    if(key < liste[mid]):
+        return dAC(liste, unten, mid-1, key)
+    else:
+        return dAC(liste, mid+1, oben, key)
         
+    
 
 
-print(dAC(liste, 10))
+unten = 0
+oben = len(liste)-1
+
+res = dAC(liste, unten, oben, 20)
+print("res:",res)
+print()
+res = dAC(liste, unten, oben, 8)
+print(res)
+print()
+print(dAC(liste, unten, oben, 0))
+print()
+print(dAC(liste, unten, oben, 3))
