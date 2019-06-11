8842c81f726fc8d447a083269723bf1a98f8368a
phisinger <philipsinger@mail.de>
2019-06-11 19:35:17 +0200

    Update Find-Funktion

--- labsolve.c
+++ labsolve.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
 #define MAXCOLS 256
 #define MAXROWS 256
@@ -17,23 +19,112 @@ typedef struct labyrinth
    int treasurey;
 }LabCell, *Lab_p;
 
-void testLab(Lab_p laby)
+// Das Labyrinth testen
+int testLab(Lab_p laby)
 {
-    for (int i = 0; i <= (laby -> maxrow); i++)
+    
+    laby -> startx = 0;
+    for (int y = 0; y <= (laby -> maxrow); y++)
     {
+        int x =0;
+        while((laby -> lab[y][x]))
+        {
+            if((laby -> lab[y][x]) =='S')
+            {
+                laby -> startx = x;
+                laby -> starty = y;
+            }
 
+            if((laby -> lab[y][x]) =='X')
+            {
+                laby -> treasurex = x;
+                laby -> treasurey = y;
+            }
+            x++;
+        }
     }
+    return (laby -> startx);
 }
 
+// Das Labyrinth ausgeben
 void printLab(Lab_p laby)
 {
-
     for (int i = 0; i <= (laby -> maxrow); i++)
     {
         printf("%s", (laby -> lab[i]));
     }
-    
-    
+}
+
+// Einen Weg durch das Labyrinth finden
+Lab_p findLab(Lab_p laby)
+{
+    int aktx = laby->startx;
+    int akty = laby->starty;
+
+    while(aktx!=laby->treasurex && akty!=laby->treasurey)
+    {
+        printf("Find aufgerufen\n");
+        printLab(laby);
+        // nach rechts laufen
+        if ((laby->lab[akty][aktx+1]) == ' ') 
+        {
+            (laby->lab[akty][aktx+1]) = '.';
+            aktx++;
+        }
+        
+        //nach unten laufen
+        else if ((laby->lab[akty+1][aktx]) == ' ')
+        {
+            (laby->lab[akty+1][aktx]) = '.';
+            akty++;
+        }
+        
+
+        // nach links laufen
+        else if ((laby->lab[akty][aktx-1]) == ' ')
+        {
+            (laby->lab[akty][aktx-1]) = '.';
+            aktx--;
+        }
+        
+        // nach oben laufen
+        else if ((laby->lab[akty-1][aktx]) == ' ')
+        {
+            (laby->lab[akty-1][aktx]) = '.';
+            akty--;
+        }
+
+        //!-Fall
+        // nach rechts laufen
+        else if ((laby->lab[akty][aktx+1]) == '.')
+        {
+            (laby->lab[akty][aktx]) = '!';
+            aktx++;
+        }
+
+        //nach unten laufen
+        else if ((laby->lab[akty+1][aktx]) == '.')
+        {
+            (laby->lab[akty][aktx]) = '!';
+            akty++;
+        }
+
+        // nach links laufen
+        else if ((laby->lab[akty][aktx-1]) == '.')
+        {
+            (laby->lab[akty][aktx]) = '!';
+            aktx--;
+        }
+
+        // nach oben laufen
+        else if ((laby->lab[akty-1][aktx]) == '.')
+        {
+            (laby->lab[akty][aktx]) = '!';
+            akty--;
+        }
+       
+    }
+    return laby;
 }
 
     int main(int argc, char *argv[])
@@ -65,9 +156,25 @@ void printLab(Lab_p laby)
     }
 
     //pruefen
-    // testLab(laby);
+    int test = testLab(laby);
+    // Debugging
+    // printf("test: %i\n", test);
+    // printf("startx: %i\n", laby -> startx);
+    // printf("starty: %i\n", laby -> starty);
+    // printf("treasurex: %i\n", laby -> treasurex);
+    // printf("treasurey: %i\n", laby -> treasurey);
 
     // Printen
+    if(testLab(laby))
+    {
+        printLab(laby);
+    }
+    else
+    {
+        printf("Das Labyrinth nicht gueltig!");
+    }
+
+    laby = findLab(laby);
     printLab(laby);
 
 
